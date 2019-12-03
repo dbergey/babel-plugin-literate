9e63e89a9919be533d9c24c4eda7b99088d6968c
Talon <hire@talon.computer>
2019-12-02 19:26:26 -0500

    use @babel/parser directly

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { declare } = require("@babel/helper-plugin-utils");
 const { dirname } = require("path");
-const parse = require("./lib/parse");
+const literate = require("./lib/parse");
+const { parse } = require("@babel/parser");
 
-module.exports = declare((babel, { files }) => {
-  babel.assertVersion(7);
+module.exports = declare(({ assertVersion }, { files }) => {
+  assertVersion(7);
   return {
     parserOverride(code, options) {
-      const opts = babel.loadOptions();
       return options.sourceFileName.match(files)
-        ? babel.parse(parse(code), opts)
-        : babel.parse(code, opts);
+        ? parse(literate(code), options)
+        : parse(code, options);
     }
   };
 });
